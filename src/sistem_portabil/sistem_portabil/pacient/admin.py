ce5b427a59b8f57a3916107f7e0dce70e4256fdf
Iulian Beleiu <iulian.beleiu@gmail.com>
2020-05-27 23:43:11 +0300

    change admin name, add localhost to allowed hosts

--- src/sistem_portabil/sistem_portabil/pacient/admin.py
+++ src/sistem_portabil/sistem_portabil/pacient/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Pacient, DateMedicale, Recomandari, Adresa
 
+admin.site.site_header = "Administrare Sistem Portabil"
 
 class Adresadmin(admin.ModelAdmin):
     list_display = ('id', 'tara', 'judet', 'localitate', 'strada', 'user')
