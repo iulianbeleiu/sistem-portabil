4592af4d6ab9ad7ecf6aa040eede6639cd7b7cf6
Iulian Beleiu <iulian.beleiu@gmail.com>
2020-05-28 19:49:12 +0300

    test endpoint

---
+++ src/sistem_portabil/sistem_portabil/pacient/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from . import views
+from .views import test
+
+urlpatterns = [
+    path('test', views.test, name='test'),
+]
--- src/sistem_portabil/sistem_portabil/pacient/views.py
+++ src/sistem_portabil/sistem_portabil/pacient/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def test(request):
+    return JsonResponse({})
